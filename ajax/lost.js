b104a6059a18b71f41c33f3cdd703f4874c1688e
Zhiyu Max Mao <zhuymao@gmail.com>
2017-04-19 14:27:46 -0700

    add more

--- ajax/lost.js
+++ ajax/lost.js
@@ -1,1 +1,10 @@
-/* TODO: Implement the lost AJAX Handler */
+/* TODO: Implement the Found AJAX Handler */
+module.exports = {
+  get: function (req, res) {
+    var data = require("./data.json");
+    res.success(data);
+  }
+  catch (ex) {
+    res.error(10, "no data");
+  }
+}
